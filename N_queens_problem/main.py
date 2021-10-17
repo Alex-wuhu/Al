7bc7139906da8808d0db37cc864b146d6367fb25
Alex <a842818832@163.com>
2021-10-17 16:13:39 +0800

    Add files via upload

--- N_queens_problem/main.py
+++ N_queens_problem/main.py
@@ -1,1 +1,23 @@
+import bfs
+import hc
+import csp
 
+
+
+if __name__ == "__main__":
+    input_file=open("./AI/input.txt",'r')
+    lines=input_file.readlines()
+
+    for line in lines:
+        n=int(line[0])
+        modle=line[2]
+        if modle=='b':
+            bfs.bfs(n)
+        elif modle=='c':
+            csp.csp(n)
+        elif modle=='h':
+            hc.hc(n)
+        else:
+            print("wrong input!!")
+    print("done!")
+    input_file.close()
